9b3a80db66f27e287376ec5088cc0a494e0cb81f
Leticia <leticiakremer24@gmail.com>
2022-09-26 00:31:27 -0300

    atualização no footer

--- aula9/www/views/templates/footer.php
+++ aula9/www/views/templates/footer.php
@@ -5,7 +5,7 @@
 
 <footer class="jumbotron text-center bg-dark">
   <p class="text-white text-center p-2 m-0">
-    ©Leticia Kremer
+    ©Djeniffer Leticia Kremer
   </p>
   <a href="admin/index.php">Área administrativa</a>
 </footer>
